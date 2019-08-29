80b6f91c4b6f2f2bb8ed36df1ef745aff7637443
ket <ket_sivak@hotmail.com>
2019-08-29 22:01:05 +0300

    week candles

--- candleValues.py
+++ candleValues.py
@@ -4,8 +4,8 @@ class candleValues:
     closeVal = {}
     hightVal = {}
     lowVal = {}
-    candles = ['min','5min', '15min', '30min', 'hour', '4hour', 'day', 'month'] #типы свечей. не трогать. TODO: переделать их так, чтоб брать из конфига. переделать во всех местах 
-    candleVal = ['open','close','hight','low'] #четыре параметра, характеризирующие одну (каждую) свечку
+    candles = ['min','5min', '15min', '30min', 'hour', '4hour', 'day', 'week', 'month'] #типы свечей. не трогать. TODO: переделать их так, чтоб брать из конфига. переделать во всех местах 
+    candleVal = ['open','close','hight','low','auth'] #первые четыре параметра, характеризирующие одну (каждую) свечку. не трогать. последний параметр характеризирует подлинность свечки (0 значит подлинный)
     candle_tmp = {} #структура следующая: для каждой свечи (типы свечей две строчки выше) записываем временно значения более мелких(читать "быстрых") свечек, чтоб потом из полученного подобия массива взять минимум\максимум для теней, и открытие\закрытие с первой\послейней мелкой свечки. значения пишем по всем четырем параметрам (см строчку выше)
 
 
@@ -27,7 +27,7 @@ class candleValues:
             self.hightVal[self.candles[i]] = c
             self.lowVal[self.candles[i]] = d
         except Exception:
-            print ("свечи бывают такие: ['min','5min', '15min', '30min', 'hour', '4hour', 'day', 'month'], а ты просишь такую: "+ str(self.candles[i]))
+            print ("свечи бывают такие: ['min','5min', '15min', '30min', 'hour', '4hour', 'day', 'week', 'month'], а ты просишь такую: "+ str(self.candles[i]))
             print (Exception)
 
     def updateTMP(self,j): #тут свежеполученную свечку добавляем в темпари (из которых потом делаем следующую), и очищаем значения текущей
@@ -47,22 +47,29 @@ class candleValues:
                 files.Logfiles['minFile'].write("incerted time " + str (y.olddata['oldtime'])+" at " + str(y.olddata['olDopenVal']) + ",   line " + str(ind) + "\n")
             for j in self.candleVal:
                 self.candle_tmp['5min'][j].append(y.candle[j]) #добавляем значений во все свечи
-            if (ind == 0): return
+            #if (ind == 0): return
             if (not ind%5):# пришло время делать пятиминутную свечку из пяти штук минутных  
                 self.updVal(self.candle_tmp['5min']['open'][0],self.candle_tmp['5min']['close'][4],max(self.candle_tmp['5min']['hight']), min(self.candle_tmp['5min']['low']),1)
                 self.updateTMP(1) # 1 means '5min'
             if (not ind%15): # пришло время делать четвертную свечку из трех штук пятиминутных
                 self.updVal(self.candle_tmp['15min']['open'][0],self.candle_tmp['15min']['close'][2],max(self.candle_tmp['15min']['hight']), min(self.candle_tmp['15min']['low']),2)
                 self.updateTMP(2) # 2 means '15min'
-            if (not ind%30): # пришло время делать получасовую свечку из двух штук минутных
-                self.updVal(self.candle_tmp['30min']['open'][0],self.candle_tmp['30min']['close'][1],max(self.candle_tmp['30min']['hight']), min(self.candle_tmp['30min']['low']),2)
+            if (not ind%30): # пришло время делать получасовую свечку из двух штук чертветных
+                self.updVal(self.candle_tmp['30min']['open'][0],self.candle_tmp['30min']['close'][1],max(self.candle_tmp['30min']['hight']), min(self.candle_tmp['30min']['low']),3)
                 self.updateTMP(3) # 3 means '30min'
             if(not ind%60): # пришло время делать часовую свечку из двух штук получасовых
-                self.updVal(self.candle_tmp['hour']['open'][0],self.candle_tmp['hour']['close'][1],max(self.candle_tmp['hour']['hight']), min(self.candle_tmp['hour']['low']),2)
+                self.updVal(self.candle_tmp['hour']['open'][0],self.candle_tmp['hour']['close'][1],max(self.candle_tmp['hour']['hight']), min(self.candle_tmp['hour']['low']),4)
                 self.updateTMP(4) # 4 means 'hour'
             if(not ind%240):# пришло время делать четырехчасовую свечку из четырех штук часовых
-                self.updVal(self.candle_tmp['4hour']['open'][0],self.candle_tmp['4hour']['close'][3],max(self.candle_tmp['4hour']['hight']), min(self.candle_tmp['4hour']['low']),2)
+                self.updVal(self.candle_tmp['4hour']['open'][0],self.candle_tmp['4hour']['close'][3],max(self.candle_tmp['4hour']['hight']), min(self.candle_tmp['4hour']['low']),5)
                 self.updateTMP(5) # 5 means '4hour'
+            if(not ind%1440):# пришло время делать дневную свечку из шести штук четырехчасовых
+                self.updVal(self.candle_tmp['day']['open'][0],self.candle_tmp['day']['close'][5],max(self.candle_tmp['day']['hight']), min(self.candle_tmp['day']['low']),6)
+                self.updateTMP(6) # 6 means 'day'
+            if(not ind%10080):# пришло время делать недельную свечку из семи штук дневных
+                self.updVal(self.candle_tmp['week']['open'][0],self.candle_tmp['week']['close'][6],max(self.candle_tmp['week']['hight']), min(self.candle_tmp['week']['low']),7)
+                self.updateTMP(7) # 7 means 'week'
+            
         except Exception:
             if (ind == 0): return #TODO придумать что-то другое
             print ("непонятная ошибка в обновлении свечей в строке почучаемого минутного файла " + str(ind))
--- main.py
+++ main.py
@@ -37,7 +37,6 @@ for line in files.source['f']:
     while ((time > oldtime or date > olddate)): #таким вот образом обнаруживается дыра в исходных минутных данных, которая заполняется предыдущими свечками
             candle.updateMe(y,j_min, files, True)
             j_min = j_min +1
-            #files.Logfiles['minFile'].write("incerted time " + str (oldtime)+" at " + str(olddate) + ",   line " + str(j_min) + "\n")
             val = updMytime(oldtime, olddate)
             y.rememberOldDatatime(y, val)
             oldtime = y.olddata['oldtime']
